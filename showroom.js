a5b3b6a27ffec2e5dfc6c609cc5c81af44541750
Giorgos Koutsikos <ragecryx@gmail.com>
2014-08-05 20:24:40 +0300

    Adds orbit camera controls, fixes AddWallSegment method, adds doc/wall_segment_vertex_indices.svg, small cleanup

--- showroom.js
+++ showroom.js
@@ -6,10 +6,12 @@ function Showroom() {
     this._aspectratio = this._width / this._height;
     this._near = 0.1;
     this._far = 10000;
+    this._worldGrid;
     
     this._defaultMaterial = new THREE.MeshLambertMaterial({color: 0xFFFFFF});
     // Wall segments
-    this._wallHeight = 3;
+    this._wallHeight = 3.05;
+    this._wallThickness = 0.3;
     this._wallSegments = [];
 
 
@@ -17,15 +19,24 @@ function Showroom() {
     this._renderer = new THREE.WebGLRenderer({antialias: true});
 
     this._previewCamera = new THREE.PerspectiveCamera( this._viewangle, this._aspectratio, this._near, this._far );
-    this._floorPlanCamera = new THREE.OrthographicCamera( this._width / - 2, this._width / 2, this._height / 2, this._height / - 2, 1, 1000 );
+    this._floorPlanCameraZoom = 32;
+    this._floorPlanCamera = new THREE.OrthographicCamera( this._width / - this._floorPlanCameraZoom, 
+                                                          this._width / this._floorPlanCameraZoom,
+                                                          this._height / this._floorPlanCameraZoom,
+                                                          this._height / - this._floorPlanCameraZoom,
+                                                          this._near, this._far );
     this._currentCamera = this._previewCamera;
     this._currentCameraName = "preview";
     
     this._scene = new THREE.Scene();
 
+    this._worldGrid = new THREE.GridHelper(20, 1);
+    this._scene.add(this._worldGrid);
+
     this._scene.add(this._currentCamera);
     this._previewCamera.position.y = 1.8;
-    this._previewCamera.position.z = 2;
+    this._previewCamera.position.x = 2;
+    this._previewCamera.position.z = 20;
     this._floorPlanCamera.position.y = 100;
     this._floorPlanCamera.lookAt(new THREE.Vector3( 0, 0, 0 ));
 
@@ -40,31 +51,16 @@ function Showroom() {
     this._pointLight.position.z = 130;
     this._scene.add(this._pointLight);
 
-    // pretty basic controls
-    function controlHandleFunc(obj){
-       return function(e){
-        //e.preventDefault();
-        if(e.which == 38) // up arrow
-            obj._currentCamera.position.z -= 1;
-        else if(e.which == 40)
-            obj._currentCamera.position.z += 1;
-        else if(e.which == 37)
-            obj._currentCamera.position.x -= 1;
-        else if(e.which == 39)
-            obj._currentCamera.position.x += 1;
-        else if(e.which == 67)
-            if(obj._currentCameraName === "preview") {
-                obj._currentCamera = obj._floorPlanCamera;
-                obj._currentCameraName = "floorplan";
-            } else {
-                obj._currentCamera = obj._previewCamera;
-                obj._currentCameraName = "preview";
-            }
-        obj.Render();
-        };
-    }
-    $(document).keydown(controlHandleFunc(this));
+    this._controls = new THREE.OrbitControls( this._currentCamera );
 
+
+    function renderAfterChangesCb(obj) {
+        return function() {
+            obj.Render();
+        }
+    }
+
+    this._controls.addEventListener('change', renderAfterChangesCb(this));
 }
 
 
@@ -100,24 +96,39 @@ Showroom.prototype.AddWallSegment = function(x1, z1, x2, z2) {
     
     // generate mesh
     var wallGeometry = new THREE.Geometry();
-    
+
+    var direction = (new THREE.Vector3(x2-x1, 0, z2-z1)).normalize();
+    var crossDirection = new THREE.Vector3();
+    crossDirection.crossVectors(direction, new THREE.Vector3(0,1,0)).normalize().multiplyScalar(this._wallThickness);
+
     wallGeometry.vertices.push( segment.startPos,
                                 segment.endPos,
                                 new THREE.Vector3(segment.startPos.x, this._wallHeight, segment.startPos.z),
-                                new THREE.Vector3(segment.endPos.x, this._wallHeight, segment.endPos.z) );
+                                new THREE.Vector3(segment.endPos.x, this._wallHeight, segment.endPos.z),
+                                (new THREE.Vector3(segment.startPos.x, 0, segment.startPos.z)).sub(crossDirection),
+                                (new THREE.Vector3(segment.endPos.x, 0, segment.endPos.z)).sub(crossDirection),
+                                (new THREE.Vector3(segment.startPos.x, this._wallHeight, segment.startPos.z)).sub(crossDirection),
+                                (new THREE.Vector3(segment.endPos.x, this._wallHeight, segment.endPos.z)).sub(crossDirection) );
     
-    wallGeometry.faces.push(new THREE.Face3(0,1,2));
-    wallGeometry.faces.push(new THREE.Face3(2,1,3));
-    
+
+    wallGeometry.faces.push( new THREE.Face3(0,1,2) );
+    wallGeometry.faces.push( new THREE.Face3(2,1,3) );
+    wallGeometry.faces.push( new THREE.Face3(2,7,6) );
+    wallGeometry.faces.push( new THREE.Face3(2,3,7) );
+    wallGeometry.faces.push( new THREE.Face3(0,2,6) );
+    wallGeometry.faces.push( new THREE.Face3(0,6,4) );
+    wallGeometry.faces.push( new THREE.Face3(5,7,3) );
+    wallGeometry.faces.push( new THREE.Face3(5,3,1) );
+    wallGeometry.faces.push( new THREE.Face3(6,5,4) );
+    wallGeometry.faces.push( new THREE.Face3(6,7,5) );
+
     wallGeometry.computeFaceNormals();
     wallGeometry.computeVertexNormals();
 
-    // test add to scene
     var wallMaterial =  new THREE.MeshLambertMaterial({color: 0xCC0000});
     var wallMesh = new THREE.Mesh(wallGeometry, this._defaultMaterial);
     this._scene.add(wallMesh);
 };
-
 
 
 Showroom.prototype.Render = function () {
